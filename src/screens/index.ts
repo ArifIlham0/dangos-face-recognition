68c281735d7bc4269cb724653ec1edb65ae8041c
ArifIlham0 <marifilham999@gmail.com>
2026-01-09 13:40:46 +0700

    feature: verifyNotAuthenticated fuction

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,7 +2,7 @@ import SplashScreen from "./initial/SplashScreen";
 import LoginScreen from "./authentication/LoginScreen";
 import RegisterScreen from "./authentication/RegisterScreen";
 import LoginFaceScreen from "./authentication/LoginFaceScreen";
-import FrontCameraScreen from "./authentication/FrontCameraScreen";
+import FrontCameraScreen from "./userFace/FrontCameraScreen";
 import HomeScreen from "./main/HomeScreen";
 import EditUserScreen from "./users/EditUserScreen";
 import UsersScreen from "./users/UsersScreen";
--- src/stores/userFaceStore.ts
+++ src/stores/userFaceStore.ts
@@ -5,11 +5,12 @@ import api from "../services/api";
 import { UserData } from "../types/user";
 import { GlobalResponse } from "../types/global";
 import { UserFaceData } from "../types/userFace";
-import { headersMultipartFormNoToken } from "../services/header";
+import { headersMultipartFormNoToken, headersMultipartFormWithToken } from "../services/header";
 
 type UserFaceState = {
     createUserFace: (image: any) => Promise<GlobalResponse<UserData>>;
     verify: (image: any) => Promise<GlobalResponse<UserFaceData>>;
+    verifyNotAuthenticated: (image: any) => Promise<GlobalResponse<UserFaceData>>;
 };
 
 const useUserFaceStore = create<UserFaceState>(() => ({
@@ -50,11 +51,49 @@ const useUserFaceStore = create<UserFaceState>(() => ({
 
     verify: async (image: any) => {
         try {
+            const user = await AsyncStorage.getItem("user");
+            const token = user ? JSON.parse(user).token : null;
+
             const formData = new FormData();
             formData.append('image', image);
             
             const response = await api.post(
                 '/user-face/verify/',
+                formData,
+                { headers: headersMultipartFormWithToken(token) },
+            );
+
+            if (response.data.status === 200) {
+                await AsyncStorage.setItem("user", JSON.stringify(response.data.data.user));
+
+                return {
+                    status: response.data.status,
+                    message: response.data.message,
+                    data: response.data.data,
+                }
+            } else {
+                return {
+                    status: response.data.status,
+                    message: response.data.message,
+                }
+            }
+            
+        } catch (error) {
+            const axiosError = error as AxiosError<{ status: number; message: string }>;
+            return {
+                status: axiosError.response?.data?.status,
+                message: axiosError.response?.data?.message,
+            }
+        }
+    },
+
+    verifyNotAuthenticated: async (image: any) => {
+        try {
+            const formData = new FormData();
+            formData.append('image', image);
+            
+            const response = await api.post(
+                '/user-face/verify-not-authenticated/',
                 formData,
                 { headers: headersMultipartFormNoToken },
             );
